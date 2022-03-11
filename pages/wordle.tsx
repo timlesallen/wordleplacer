2423b64f70fd59f782e3077905d05fd61c2b9f85
Tim Allen <tim.allen@marketplacer.com>
2022-03-11 12:14:27 +1100

    passing something a bit useful to board now.

--- pages/wordle.tsx
+++ pages/wordle.tsx
@@ -1,4 +1,5 @@
 import Board from '../components/Board'
+import { BoardType } from '../components/Board'
 import Head from 'next/head'
 import Image from 'next/image'
 import Keyboard from '../components/Keyboard'
@@ -6,18 +7,41 @@ import React, { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 import type { NextPage } from 'next'
 
+const BOARD_WIDTH = 5;
+const MAX_GUESSES = 6;
 interface State {
   wordToGuess: string,
   guessesSoFar: string[],
   currentGuess: string
 }
 
+function boardFromState (state: State) {
+  const board = emptyBoard();
+  state.guessesSoFar.forEach((guess: string, row: number) => {
+    var col = 0;
+    for (let c of guess) {
+      board[row][col].letter = c;
+      col++;
+    }
+  });
+  return board;
+}
+
+function emptyBoard () {
+  return new Array(MAX_GUESSES).fill(null).map(() =>
+    new Array(BOARD_WIDTH).fill(null).map(() => ({ letter: '', color: 'white' }))
+  );
+}
+
 function debugState (state: State) {
   console.log(state);
+  console.log(boardFromState(state));
 }
 
 const Home: NextPage = () => {
+  // FIXME wordToGuess selected from list...
   const [state, setState] = useState<State>({ wordToGuess: 'pause', guessesSoFar: [], currentGuess: '' });
+  var board: BoardType = emptyBoard();
 
   const onKeyPress = (key: string) => {
     const { wordToGuess, guessesSoFar, currentGuess } = state;
@@ -30,11 +54,12 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     debugState(state);
+    board = boardFromState(state);
   });
 
   return (
     <div className={styles.container}>
-      <Board></Board>
+      <Board board={boardFromState(state)}></Board>
       <Keyboard onKeyPress={onKeyPress}></Keyboard>
     </div>
   )
